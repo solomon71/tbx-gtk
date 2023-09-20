899b434b471f096c87606b9c218116f7df95a2e4
Kyle Cummings <kyle.cummings@thinkingbox.com>
2023-09-20 19:14:02 -0400

    code-simple block

---
+++ blocks/code-simple/code-simple.js
@@ -0,0 +1,12 @@
+import createTag from '../../utils/tag.js';
+
+export default function decorate(block) {
+  const code = block.querySelector('pre code');
+  const lines = code.textContent.split('\n');
+  const div = document.createElement('div');
+  lines.filter(line => line.length > 0).forEach((line) => {
+    const span = createTag('span', {}, line);
+    div.appendChild(span);
+  });
+  code.innerHTML = div.innerHTML;
+}
